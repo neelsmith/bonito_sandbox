9fa9e615247413c9ca5c6294fac7b8d218b09da4
Neel Smith <dnsmith.neel@gmail.com>
2023-12-26 14:51:31 -0500

    save grid

--- 3b_grid_clustered.jl
+++ 3b_grid_clustered.jl
@@ -1,41 +1,56 @@
 using Bonito
 Bonito.browser_display()
 
-
 """Create a `Card` with background set to highlight grid structure
 """
 function demoCard(content=DOM.div(); style=Styles(), attributes...) 
     Card(content; backgroundcolor=:silver, border_radius="2px", style=Styles(style, "color" => :white),attributes...)
 end
 
-
-"""Create a `Grid` with content in two columns.
-"""
-function griddeddiv(button, s, clicks)
-    Grid(
-        demoCard(button),
-        demoCard( Bonito.Label(clicks));
-   
-        height="300px", columns="65% 35%"
-    )
-end
-
 # Put some live widgets into the grid.
-drop = Dropdown(["septuagint", "masoretic", "vulgate"])
 button = Button("Bump counter")
 clicks = Observable(0)
 
-g = griddeddiv(button, drop, clicks)
-typeof(g)
-
-# Build app with live updating of content.
-app3 = App() do session
-    on(session, button.value) do bool
-        clicks[] = clicks[] + 1
-    end
-    return(g)
+function gridded()
 end
-display(app3)
+
+
+ app3b =   App() do
+        sidebar = demoCard(
+            "SIDEBAR",
+            style = Styles(
+                "grid-column" =>  "1",
+                "grid-row" =>  "1 / 3",
+            )
+        )
+
+
+        header = demoCard(
+            "HEADER",
+            style = Styles(
+                "grid-column" =>  "2",
+                "grid-row" =>  "1",
+            )
+        )
+
+
+        main = demoCard(
+            "MAIN",
+            style = Styles(
+                "grid-column" =>  "2",
+                "grid-row" =>  "2",
+            )
+        )
+
+
+        grid = Grid(
+            sidebar, header, main,
+            columns = "2fr 5fr",
+            rows = "50px 1fr"
+        )
+    return DOM.div(grid; style=Styles("height" => "600px", "margin" => "20px", "position" => :relative))
+end
 
 
 
+display(app3b)
