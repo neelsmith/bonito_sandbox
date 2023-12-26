f54d4e664630f7c805d5000a5e2c0578ddc25b2b
Neel Smith <dnsmith.neel@gmail.com>
2023-12-26 15:54:00 -0500

    Add complex grid lahyout

--- 3b_grid_clustered.jl
+++ 3b_grid_clustered.jl
@@ -7,50 +7,53 @@ function demoCard(content=DOM.div(); style=Styles(), attributes...)
     Card(content; backgroundcolor=:silver, border_radius="2px", style=Styles(style, "color" => :white),attributes...)
 end
 
+function gridded(button, clicks)
+    sidebar = demoCard(
+        #"SIDEBAR",
+        button,
+        style = Styles(
+            "grid-column" =>  "1",
+            "grid-row" =>  "1 / 3",
+        )
+    )
+
+    header = demoCard(
+        #"HEADER",
+        Bonito.Label(clicks),
+        style = Styles(
+            "grid-column" =>  "2",
+            "grid-row" =>  "1",
+        )
+    )
+
+    main = demoCard(
+        "MAIN",
+        style = Styles(
+            "grid-column" =>  "2",
+            "grid-row" =>  "2",
+        )
+    )
+
+    grid = Grid(
+        sidebar, header, main,
+        columns = "2fr 5fr",
+        rows = "50px 1fr"
+    )
+    return grid
+end
+
+
 # Put some live widgets into the grid.
 button = Button("Bump counter")
 clicks = Observable(0)
+g = gridded(button, clicks)
 
-function gridded()
+app3b = App() do session
+    on(session, button.value) do bool
+        clicks[] = clicks[] + 1
+    end
+    return(g)
 end
-
-
- app3b =   App() do
-        sidebar = demoCard(
-            "SIDEBAR",
-            style = Styles(
-                "grid-column" =>  "1",
-                "grid-row" =>  "1 / 3",
-            )
-        )
-
-
-        header = demoCard(
-            "HEADER",
-            style = Styles(
-                "grid-column" =>  "2",
-                "grid-row" =>  "1",
-            )
-        )
-
-
-        main = demoCard(
-            "MAIN",
-            style = Styles(
-                "grid-column" =>  "2",
-                "grid-row" =>  "2",
-            )
-        )
-
-
-        grid = Grid(
-            sidebar, header, main,
-            columns = "2fr 5fr",
-            rows = "50px 1fr"
-        )
-    return DOM.div(grid; style=Styles("height" => "600px", "margin" => "20px", "position" => :relative))
-end
-
-
 
 display(app3b)
+
